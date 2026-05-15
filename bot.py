4cae06b55f18607e3022c0054ac6f5402002ba28
𝐂𝐑𝐈𝐌𝐄𝐗 𝐓𝐄𝐀𝐌 𝙳𝚎𝚟 (চন্দ্রবিন্দুর চাঁদ) <moonx9572@gmail.com>
2026-05-16 01:14:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ const Database = require("better-sqlite3");
 const path = require("path");
 
 // ===================== CONFIG =====================
-const TOKEN = "8756023029:AAECl1GHT2OMbXaprjyg-jFfvQ1N_OPUOv0";
+const TOKEN = "8756023029:AAFd9rUcKNqGHF-wBAEe0BIxxCIA83RSieI";
 const ADMIN_ID = 5924662015;
 
 // ── Daily Claim ──
